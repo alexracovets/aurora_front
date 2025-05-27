afacb657464bae336db86bba5f761900fda2c574
Oleksandr <alexracovets@gmail.com>
2025-05-27 22:17:23 +0300

    test

---
+++ src/app/(payload)/admin/importMap.js
@@ -0,0 +1,5 @@
+import { Dashboard as Dashboard_535a35486f98f4a0498329220119eba6 } from '../../../app/views/Dashbord'
+
+export const importMap = {
+  "./app/views/Dashbord#Dashboard": Dashboard_535a35486f98f4a0498329220119eba6
+}
