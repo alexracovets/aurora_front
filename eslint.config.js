5c48a793fede3dcaacaccf0d8e56183116e3b866
Oleksandr <alexracovets@gmail.com>
2025-05-27 22:31:52 +0300

    fix fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,17 @@
-import { nextJsConfig } from "@repo/eslint-config/next-js";
+import nextPlugin from '@eslint/eslintrc';
+import nextConfig from 'eslint-config-next';
 
 /** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
+export default [
+  {
+    ignores: ['node_modules/**', '.next/**', 'out/**'],
+  },
+  ...nextPlugin.configs.recommended,
+  ...nextConfig,
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  },
+];
