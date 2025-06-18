24969840854e0219a836cc21ed1ef5002e28f40b
Oleksandr <alexracovets@gmail.com>
2025-06-18 17:28:53 +0300

    test

--- src/ui/components/organisms/Header/Header.tsx
+++ src/ui/components/organisms/Header/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Headroom from "react-headroom";
 
 import { Container, Logo } from "@atoms";
 import { HeaderNavigation } from "@molecules";
@@ -12,19 +11,17 @@ export const Header = () => {
     const { navigation } = useNavigationStore();
 
     return (
-        <Headroom>
-            <header className={cn(
-                "shadow-md bg-white",
-                "translate-y-[-100%] transition-all duration-300",
-                navigation.length > 0 && "translate-y-0"
-            )}>
-                <Container className="flex items-center gap-x-[16px] py-[12px]">
-                    <Link href="/">
-                        <Logo />
-                    </Link>
-                    <HeaderNavigation />
-                </Container>
-            </header>
-        </Headroom>
+        <header className={cn(
+            "shadow-md bg-white",
+            "translate-y-[-100%] transition-all duration-300",
+            navigation.length > 0 && "translate-y-0"
+        )}>
+            <Container className="flex items-center gap-x-[16px] py-[12px]">
+                <Link href="/">
+                    <Logo />
+                </Link>
+                <HeaderNavigation />
+            </Container>
+        </header>
     )
 }; 
